9367b16beecbb2ea16de8e9e7439d34e4e0d366e
myomi <myomit@gmail.com>
2017-06-08 08:16:59 +0900

    Added sass bundle.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./index.html";
+import "./scss/index.scss";
 import Elm from "./elm/Main";
 
 Elm.Main.embed(document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,26 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Stylelint = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: 'app.css'
+});
+
+const stylelint = new Stylelint();
+
+const html = new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'src/index.html',
+    hash: true
+});
+
+const include = new HtmlWebpackIncludeAssetsPlugin({
+    assets: [],
+    append: false,
+    hash: true
+});
 
 module.exports = {
     entry: './src/index.js',
@@ -15,15 +37,56 @@ module.exports = {
                 options: {}
             }
         }, {
-            test: /\.html$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]'
-                }
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }],
+                // use style-loader in development
+                fallback: 'style-loader'
+            })
+        }, {
+            test: /\.(eot|woff(2)?)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                mimetype: 'image/svg+xml'
+            }
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 10000,
+                mimetype: 'application/octet-stream'
+            }
+        }, {
+            test: /.(jpg|jpeg|png)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
             }
         }]
     },
+    plugins: [
+        stylelint,
+        extractSass,
+        html,
+        include
+    ],
     resolve: {
         extensions: ['.js', '.elm']
     }

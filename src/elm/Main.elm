e40c1a7fae552b50c79e62eb420620a5bcc18de4
myomi <myomit@gmail.com>
2017-06-08 20:38:09 +0900

    Added sample code

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,41 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
-main : Html.Html a
+
+main : Program Never Model Msg
 main =
-    text "hello"
+    Html.beginnerProgram
+        { model = 0
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
